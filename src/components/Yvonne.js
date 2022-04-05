f84dcd2bd7bf981e1bfc20e78353c4e3b7a161d9
Amy Lim H L <66409683+Ocean272@users.noreply.github.com>
2022-04-05 22:43:07 +0800

    Trying out chart.js on Yvonne.js

--- src/components/Yvonne.js
+++ src/components/Yvonne.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
+//import { BasePlatform } from 'chart.js';
 
 const Yvonne = ({data}) => {
 
@@ -9,13 +10,13 @@ const Yvonne = ({data}) => {
 
   function search(data) {
     return data.filter((item) => {
-      if (item.pr_date === filterParam) {
+      if (item.pr_date == filterParam) {
         return searchParamLC.some((newItem) => {
           return (
             item[newItem].toString().toLowerCase().indexOf(r.toLowerCase()) > -1
           );
         });
-      } else if (filterParam === "All") {
+      } else if (filterParam == "All") {
         return searchParamLC.some((newItem) => {
           return (
             item[newItem].toString().toLowerCase().indexOf(r.toLowerCase()) > -1
@@ -29,6 +30,7 @@ const Yvonne = ({data}) => {
     <>
     <h2>Number of Local Cases based on </h2>
     <div className="wrapper">
+     
         <label htmlFor="search-form">
           <input
             type="search"
@@ -62,6 +64,58 @@ const Yvonne = ({data}) => {
             </div>
           );
         })}
+         {/* <BasePlatform
+        data={{
+          labels: ["Red","Blue","Yellow","Green","Orange"],
+          datasets: [
+            {
+            label: 'no of Cases',
+            data: [2,3,4,5,6,7],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ]
+            borderWidth: 1;
+            },
+            {
+            label: 'as of date',
+            data: [3,4,5,6,7,8],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            }
+          ]
+        }}
+        height={400}
+        width={600}
+        options={{maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              }
+            }
+          ]
+        }}}
+      /> */}
       </div>
       {/* {data.map((a) => {
         return (
