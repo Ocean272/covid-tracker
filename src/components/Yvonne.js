0c4429716fa8c33f5d3129c2262d8a9c8ae015a2
Amy Lim H L <66409683+Ocean272@users.noreply.github.com>
2022-04-05 13:17:45 +0800

    Edited 5_April_22_1317_Amy

--- src/components/Yvonne.js
+++ src/components/Yvonne.js
@@ -1,10 +1,69 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Yvonne = ({data}) => {
+
+  const [r, setR] = useState("");
+  const [searchParamLC] = useState(["pr_date"]);
+  const [filterParam, setFilterParam] = useState(["All"]);
+
+  function search(data) {
+    return data.filter((item) => {
+      if (item.pr_date == filterParam) {
+        return searchParamLC.some((newItem) => {
+          return (
+            item[newItem].toString().toLowerCase().indexOf(r.toLowerCase()) > -1
+          );
+        });
+      } else if (filterParam == "All") {
+        return searchParamLC.some((newItem) => {
+          return (
+            item[newItem].toString().toLowerCase().indexOf(r.toLowerCase()) > -1
+          );
+        });
+      }
+    });
+  }
+      
   return (
     <>
-    <h3>Number of local cases by Age</h3>
-      {data.map((a) => {
+    <h2>Number of Local Cases based on </h2>
+    <div className="wrapper">
+        <label htmlFor="search-form">
+          <input
+            type="search"
+            name="search-form"
+            id="search-form"
+            className="search-input"
+            placeholder="YYYY-MM-DD"
+            value={r}
+            onChange={(e) => setR(e.target.value)}
+          />
+        </label>
+      </div>
+      <span> Search Date</span>
+      <label
+        onChange={(e) => {
+          setFilterParam(e.target.value);
+        }}
+        aria-label="Date"
+      >
+        <option value="All"></option>
+      </label>
+      <div>
+
+        {search(data).map((a) => {
+          return (
+            <div key={a._id}>
+              <p>Date : {a.pr_date}</p>
+              <p>Age group : {a.age_group}</p>
+              <p>No of cases : {a.count_of_case}</p>
+              <br />
+            </div>
+          );
+        })}
+      </div>
+      {/* {data.map((a) => {
         return (
           <div key={a._id}>
             <p>Count of Case : {a.count_of_case}</p>
@@ -13,9 +72,9 @@ const Yvonne = ({data}) => {
             <br/>
           </div>
         );
-      })}
+      })} */}
     </>
   )
 }
 
-export default Yvonne
+export default Yvonne;
