ecff3571d3249a41b0680bd75143273dfd8d9d6c
Ocean272 <ocean272@hotmail.com>
2022-04-07 14:47:21 +0800

    remove unnecessary comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import Art from "../componentsTwo/ART";
 import API from "../screens/API";
 import ART from "../componentsTwo/ART.js";
 import Support from "../componentsTwo/Support";
@@ -11,6 +10,14 @@ function Overview() {
   const [data3, setData3] = useState([]);
   const [data4, setData4] = useState([]);
 
+  useEffect(() => {
+    getAsyncData1();
+    getAsyncData2();
+    getAsyncData3();
+    getAsyncData4();
+  }, []);
+
+
   const getAsyncData1 = async () => {
     const resp = await API.get(
       "/api/action/datastore_search?resource_id=6c14814b-09b7-408e-80c4-db3d393c7c15"
@@ -20,22 +27,25 @@ function Overview() {
       setData1(resp.data.result.records[0]);
     }
   };
-  useEffect(() => {
-    getAsyncData1();
-  }, []);
+  const getAsyncData2 = async () => {
+    const resp = await API.get(
+      "/api/action/datastore_search?resource_id=9ec89dc0-cb6b-4604-aaff-382d5e850206"
+    );
+    if (resp.status === 200) {
+      console.log(resp);
+      setData2(resp.data.result.records[0]);
+    }
+  };
 
   const getAsyncData3 = async () => {
     const resp = await API.get(
       "/api/action/datastore_search?resource_id=783f0c4c-caf7-4818-8683-760f3d7f0757"
     );
     if (resp.status === 200) {
-      console.log(resp.data.result.records[0]);
-      setData3(resp.data.result.records[0]);
+      console.log(resp);
+      setData3(resp.data.result.records[6]);
     }
   };
-  useEffect(() => {
-    getAsyncData3();
-  }, []);
 
   const getAsyncData4 = async () => {
     const resp = await API.get(
@@ -46,22 +56,6 @@ function Overview() {
       setData4(resp.data.result.records[0]);
     }
   };
-  useEffect(() => {
-    getAsyncData4();
-  }, []);
-
-  const getAsyncData2 = async () => {
-    const resp = await API.get(
-      "/api/action/datastore_search?resource_id=9ec89dc0-cb6b-4604-aaff-382d5e850206"
-    );
-    if (resp.status === 200) {
-      console.log(resp);
-      setData2(resp.data.result.records[0]);
-    }
-  };
-  useEffect(() => {
-    getAsyncData2();
-  }, []);
 
   return (
     <>
@@ -80,22 +74,37 @@ function Overview() {
           </tr>
         </tbody>
         <tbody>
-          <h3>Deceased Overview</h3>
+          <h3>Vaccination Overview</h3>
           <tr>
-            <th>Count of Case</th>
-            <th>Vaccination Status</th>
-            <th>As of Date</th>
-            <th>Clinical Status</th>
+            <th>Completed Full Regimen</th>
+            <th>Age group</th>
+            <th>Unvaccinated</th>
+            <th>At least One Dose </th>
+          </tr>
+          <tr key={data2._id}>
+            <td>{data2.completed_full_regimen}</td>
+            <td>{data2.age}</td>
+            <td>{data2.unvaccinated}</td>
+            <td>{data2.at_least_one_dose}</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <h3>Number in ICU Overview</h3>
+          <tr>
+            <th>No of cases in percentage</th>
+            <th>Date</th>
+            <th>Vaccination status</th>
+            <th>Health Status</th>
           </tr>
           <tr key={data3._id}>
             <td>{data3.count_of_case}</td>
-            <td>{data3.vaccination_status}</td>
             <td>{data3.as_of_date}</td>
+            <td>{data3.vaccination_status}</td>
             <td>{data3.clinicalstatus}</td>
           </tr>
         </tbody>
         <tbody>
-          <h3>Recovered Overview</h3>
+          <h3>Hospitalization Overview</h3>
           <tr>
             <th>Status</th>
             <th>As of Date</th>
@@ -107,21 +116,7 @@ function Overview() {
             <td>{data4.value}</td>
           </tr>
         </tbody>
-        <tbody>
-          <h3>Discharged Overview</h3>
-          <tr>
-            <th>completed full regimen</th>
-            <th>Age Group</th>
-            <th>Unvaccinated</th>
-            <th>At least one dose</th>
-          </tr>
-          <tr key={data2._id}>
-            <td>{data2.completed_full_regimen}</td>
-            <td>{data2.age}</td>
-            <td>{data2.unvaccinated}</td>
-            <td>{data2.at_least_one_dose}</td>
-          </tr>
-        </tbody>
+        
       </table>
 
       <br />
